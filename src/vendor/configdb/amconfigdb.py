1feb70c975d0483cde15b4d05352803326fcd824
Tom Mitchell <tmitchell@bbn.com>
2015-04-07 16:57:16 -0400

    Fix a SQLAlchemy exception.
    SQLAlchemy raises exceptions saying "ProgrammingError: SQLite objects
    created in a thread can only be used in that same thread.The object
    was created in thread ...". Some investigation indicates that this
    involves the AMsoil configdb, which is a SQLite database.
    
    Add the 'check_same_thread': False connection argument when creating
    the SQLAlchemy engine to avoid the issue. We think this is ok because
    the configdb is a readonly database for chapi.

--- src/vendor/configdb/amconfigdb.py
+++ src/vendor/configdb/amconfigdb.py
@@ -14,7 +14,7 @@ import amsoil.core.log
 logger=amsoil.core.log.getLogger('configdb')
 
 # initialize sqlalchemy
-db_engine = create_engine(CONFIGDB_ENGINE, pool_recycle=6000) # please see the wiki for more info
+db_engine = create_engine(CONFIGDB_ENGINE, connect_args={'check_same_thread':False}, pool_recycle=6000) # please see the wiki for more info
 db_session_factory = sessionmaker(autoflush=True, bind=db_engine, expire_on_commit=False) # the class which can create sessions (factory pattern)
 db_session = scoped_session(db_session_factory) # still a session creator, but it will create _one_ session per thread and delegate all method calls to it
 # we could limit the session's scope (lifetime) to one request, but for this plugin this is not necessary
